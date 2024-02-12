993f36d2971594abe2c62f3c1318676d38f520bb
prmorais <prmorais1302@gmail.com>
2024-02-12 20:41:23 -0300

    Creating counter page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'counter',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     loadComponent: ()=> import('./pages/home/home.component')
   },
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.component')
+  },
+  {
+    path: 'counter',
+    loadComponent: () => import('./counter/counter.component').then(c => c.CounterComponent)
   }
 ];
